f9ab28d0af0052e1752d6a7f08df5a88f97aa898
Usman Sarwar <165565408+USSMANsarwar1024@users.noreply.github.com>
2025-05-28 05:23:35 -0700

    Update restart_tomcat.sh

--- restart_tomcat.sh
+++ restart_tomcat.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Make sure scripts are executable (once only — not always needed)
-sudo chmod +x /opt/tomcat/bin/*.sh
-sudo su
-# Gracefully restart Tomcat
-cd /opt/tomcat/bin
-./shutdown.sh
-sleep 5
-
-cd /opt/tomcat/bin/
-./startup.sh
+sudo systemctl restart tomcat
